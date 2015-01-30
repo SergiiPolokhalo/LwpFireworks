4f1b423c3f5db367ea1d63c2cee48bd3c505383f
Serhii Polokhalo <kitsonkit@gmail.com>
2015-01-30 09:51:47 +0200

    fix lost files

---
+++ android/src/ua/pp/kitson/trf/android/LWPFireworksService.java
@@ -0,0 +1,27 @@
+package ua.pp.kitson.trf.android;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.badlogic.gdx.backends.android.AndroidLiveWallpaperService;
+import com.badlogic.gdx.backends.android.AndroidWallpaperListener;
+
+import ua.pp.kitson.trf.ThrRealFireworks;
+
+/**
+ * Created by serhii on 1/29/15.
+ */
+public class LWPFireworksService extends AndroidLiveWallpaperService {
+    @Override
+    public void onCreateApplication() {
+        super.onCreateApplication();
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+//        config.useGL20 = false;
+        config.useCompass = false;
+        config.useWakelock = false;
+        config.useAccelerometer = false;
+        config.getTouchEventsForLiveWallpaper = true;
+
+        ApplicationListener listener = new ThrRealFireworks();
+        initialize(listener, config);
+    }
+ }
